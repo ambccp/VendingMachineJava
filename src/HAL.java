34e9650b2e9ff717d78e3061fdc8c247b950019f
António Manuel Bacao Pereira <ambccp@gmail.com>
2018-10-20 12:01:21 +0100

    Merging code

--- src/HAL.java
+++ src/HAL.java
@@ -1,27 +1,44 @@
 import isel.leic.UsbPort;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-/**
- * @author pereiraa
- *
- */
 public class HAL {
+    private static final int DVAL_BIT = 16; // Bit que representa o sinal dVal que indica existência de dados no barramento de Keyboard Reader
 
-  /**
-   * @param args
-   */
-  public static void init() {
-    
-  }
-  
-  
-  
-  public static void main(String[] args) {
-    UsbPort.out(1);
-    System.out.println(UsbPort.in());
-    int mask = 245&16>>2;
-    Integer.toBinaryString(mask);
-    System.out.println(Integer.toBinaryString(mask));
-    
-  }
+    public static void main(String[] args) {
+//        UsbPort.out(1);
+//        UsbPort.out(0);
+//        System.out.println(UsbPort.in());
+        System.out.println(0 & 15);
+    }
+
+    // Inicia a classe
+    public static void init() {
+        throw new NotImplementedException();
+    }
+
+    // Retorna true se o bit tiver o valor lógico ‘1’
+    public static boolean isBit(int mask) {
+        return (mask & UsbPort.in()) == mask;
+    }
+
+    // Retorna os valores dos bits representados por mask presentes no UsbPort
+    public static int readBits(int mask) {
+        return mask & UsbPort.in();
+    }
+
+    // Escreve nos bits representados por mask o valor de value
+    public static void writeBits(int mask, int value) {
+        UsbPort.out(mask & value);
+    }
+
+    // Coloca os bits representados por mask no valor lógico ‘1’
+    public static void setBits(int mask) { // ler do in?
+        UsbPort.out(mask | 0);
+    }
+
+    // Coloca os bits representados por mask no valor lógico ‘0’
+    public static void clrBits(int mask) {
+        UsbPort.out(mask ^ 0); // ler do in?
+    }
 
 }
