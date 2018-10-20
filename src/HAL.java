255895b727c590ea047a08f84e1c49638fe639f1
António Manuel Bacao Pereira <ambccp@gmail.com>
2018-10-20 11:14:39 +0100

    Initial Commit
    Adding KBD calss

---
+++ src/HAL.java
@@ -0,0 +1,27 @@
+import isel.leic.UsbPort;
+
+/**
+ * @author pereiraa
+ *
+ */
+public class HAL {
+
+  /**
+   * @param args
+   */
+  public static void init() {
+    
+  }
+  
+  
+  
+  public static void main(String[] args) {
+    UsbPort.out(1);
+    System.out.println(UsbPort.in());
+    int mask = 245&16>>2;
+    Integer.toBinaryString(mask);
+    System.out.println(Integer.toBinaryString(mask));
+    
+  }
+
+}
---
+++ src/KBD.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author pereiraa
+ *
+ */
+public class KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public static final char NONE = 0;
+ 
+  // Inicia a classe
+  public static void init() {
+    
+  }
+  // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
+  public static char getKey() {
+    return 0;
+    
+  }
+  // Retorna quando a tecla for premida ou NONE após decorrido ‘timeout’ milisegundos.
+  public static char waitKey(long timeout) {
+    return 0;}
+  
+}
