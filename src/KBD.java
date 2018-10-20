df492cda498b557a31e01df22b122f1918c9754e
António Manuel Bacao Pereira <ambccp@gmail.com>
2018-10-20 15:47:34 +0100

    Choosing mask

--- src/KBD.java
+++ src/KBD.java
@@ -13,35 +13,39 @@ public class KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
 
   // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
   public static char getKey() {
-    int value = HAL.readBits(256);
-    final int dval = HAL.DVAL_BIT; 
-    switch (value) {
-      case (dval+1) :
-        return '1';
-      case (dval+2) :
-        return '2';
-      case (dval+3) :
-        return '3';
-      case (dval+4) :
-        return '4';
-      case (dval+5) :
-        return '5';
-      case (dval+6) :
-        return '6';
-      case (dval+7) :
-        return '7';
-      case (dval+8) :
-        return '8';
-      case (dval+9) :
-        return '9';
-      case (dval+11) :
-        return '0';
-      case (dval+10) :
-        return '*';
-      case (dval+12) :
-        return '#';
-      default :
-        return NONE;
+    int value = HAL.readBits(255); //31 reads dval + data(0..3)
+    final int dval = HAL.DVAL_BIT;
+    if (value>=dval) { //not using getBit() to avoid a new reading from software
+      switch (value) {
+        case (dval + 0) : //int value used explicit 
+          return '1';
+        case (dval + 1) :
+          return '4';
+        case (dval + 2) :
+          return '7';
+        case (dval + 3) :
+          return '*';
+        case (dval + 4) :
+          return '2';
+        case (dval + 5) :
+          return '5';
+        case (dval + 6) :
+          return '8';
+        case (dval + 7) :
+          return '0';
+        case (dval + 8) :
+          return '3';
+        case (dval + 9) :
+          return '6';
+        case (dval + 10) :
+          return '9';
+        case (dval + 11) :
+          return '#';
+        default :
+          return NONE;
+      }
+    } else {
+      return NONE;
     }
   }
   // Retorna quando a tecla for premida ou NONE após decorrido ‘timeout’
